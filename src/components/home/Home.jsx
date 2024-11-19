9fba4bb6669ebb8d4d7b50035b9348ad1ed1a56a
SMGolamAlif <golamalif4702@gmail.com>
2024-11-20 00:38:05 +0600

    code update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -27,8 +27,8 @@ function Home() {
   return (
     <div className="min-h-screen bg-gray-50"> <Navbar />
       <MenuSearch/>
-      <HomeLanding/>
-      <AboutSection/>
+      {/* <HomeLanding/> */}
+      {/* <AboutSection/> */}
       <div className="container mx-auto px-4 py-8">
        
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
